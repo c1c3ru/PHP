d9ff1cc34f0fc9fe4fa9ba0e0b827481a6733e86
c1c3ru <35180539+c1c3ru@users.noreply.github.com>
2018-01-19 14:07:27 -0300

    Add files via upload

--- Cadastro.php
+++ Cadastro.php
@@ -2,99 +2,19 @@
 /**
  * Created by IntelliJ IDEA.
  * User: cti
- * Date: 18/01/18
- * Time: 09:52
+ * Date: 19/01/18
+ * Time: 11:52
  */
 
-class Cadastro
+
+namespace Cliente;
+
+class Cadastro extends \Cadastro
 {
 
-    private $nome;
-    private $email;
-    private $senha;
+    public function registraVenda(){
 
-
-    public function getNome(): string{
-
-        return $this->nome;
-
-    }
-
-    public function getEmail():string{
-
-        return $this->email;
-
-    }
-
-    public function getSenha():string{
-
-        return $this->senha;
-
-    }
-
-    public function setNome($nome){
-
-        return $this->nome = $nome;
-
-    }
-
-    public function setEmail($email){
-
-        return $this->email = $email;
-
-    }
-
-    public function setSenha($senha){
-
-        return $this->senha = $senha;
-
-    }
-
-    public function  __toString()
-    {
-        // TODO: Implement __toString() method.
-        return jason_encode(array(
-            "nome"=>$this->getNome(),
-            "email"=>$this->getEmail(),
-            "senha"=>$this->getSenha()
-        ));
+        echo "registrado!!". $this->getNome();
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
